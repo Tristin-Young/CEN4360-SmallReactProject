cb78980b45e8cd28070030bb0f1a6956477004b1
Tristin-Young <86166175+Tristin-Young@users.noreply.github.com>
2024-01-10 04:14:28 -0500

    Small React Project
    This is the core functionality of the React project. The project creates a form that
    a user can fill out. Once filled out, the user's entries are relayed back to them through
    an alert command. The entries are seemingly stored in two sets of useState() Hooks, but that
    is somewhat misleading. The first set of hooks is utilized to set the form field values in the
    alert call. After the hooks have been parsed into the call, they are reinitialized so the user
    can fill the form out again. The second set of hooks persists the form field values and does
    not get reinitialized. This is how the recall button operates.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import "./Form.js"
+import Form from './Form.js';
+function App() {
+  return (
+    <>
+    <h1>Small React App Assignment-CEN4360</h1>
+    <Form />
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,77 @@
+// Form handling
+import "./Form.css";
+import { useState } from "react";
+
+function Form() {
+
+    // controlled components for the form fields (name, email, zip code)
+    // these are the values that will be submitted with the form
+    // once they are submitted, the fields should be cleared
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [zip, setZip] = useState("");
+
+    // because the above fields are going to be cleared after submission
+    // we need to keep track of the values that were submitted
+    // this is what the following three states are for
+    const [submittedName, setSubmittedName] = useState("");
+    const [submittedEmail, setSubmittedEmail] = useState("");
+    const [submittedZip, setSubmittedZip] = useState("");
+
+    // function to handle the form submission
+    // the parameter e is the event object
+    const submit = (e) => {
+        // prevent the default form submission behavior
+        e.preventDefault();
+        // set the submitted values to the current values of the controlled components
+        setSubmittedName(name);
+        setSubmittedEmail(email);
+        setSubmittedZip(zip);
+        //alert the user of their inputs
+        alert(`Name: ${name}\nEmail: ${email}\nZip Code: ${zip}`);
+        // clear the controlled components
+        setName("");
+        setEmail("");
+        setZip("");
+    };
+
+    // function to call back the last set of submitted values
+    const recall = () => {
+        alert(`Name: ${submittedName}\nEmail: ${submittedEmail}\nZip Code: ${submittedZip}`);
+    };
+
+    // return the form element
+    //each input has an onChange event handler that updates the corresponding state
+    //the submit button has an onClick event handler that calls the submit function
+    //the recall button has an onClick event handler that calls the recall function
+    return (
+        <form>
+            <input
+                value={name}
+                type="text"
+                placeholder="John Doe"
+                onChange={(event) => setName(event.target.value)}
+            />
+            <input
+                value={email}
+                type="text"
+                placeholder="johndoe123@gmail.com"
+                onChange={(event) => setEmail(event.target.value)}
+            />
+            <input
+                value={zip}
+                type="text"
+                placeholder="12345"
+                onChange={(event) => setZip(event.target.value)}
+            />
+            <button onClick={submit}>Register</button>
+            <button onClick={recall}>Recall</button>
+        </form>
+
+    );
+
+
+}
+
+// export the Form component
+export default Form;
